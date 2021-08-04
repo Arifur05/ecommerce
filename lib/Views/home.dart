48bca31283b6c806273d3bf5bd3a108887fae310
Arifur Rashid <arifrashid06@gmail.com>
2021-08-05 00:57:58 +0600

    home page created

---
+++ lib/Views/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => HomePage();
+  }
+
+class HomePage extends State<Home>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
