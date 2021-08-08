df49722b740a5793b6bcc37b0960d159a55cf341
Arifur Rashid <arifrashid06@gmail.com>
2021-08-09 01:26:31 +0600

    pages routes

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -30,46 +30,67 @@ class MainScreen extends State<Home> {
         backgroundColor: Colors.white,
         bottomNavigationBar: Row(
           children: [
-            buildBottomNav("images/home.svg", 0, onClicked: ),
+            buildBottomNav("images/home.svg", 0),
             buildBottomNav("images/menu.svg", 1),
             buildBottomNav("images/shopping-cart.svg", 2),
             buildBottomNav("images/user.svg", 3),
+          ],
+        ),
+        body: SafeArea(
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: _onPageChange,
+            children: _pages,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildBottomNav(
+    String icon,
+    int index,
+    //VoidCallback? onClicked
+  ) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          _selectedItem = index;
+          _pageController.jumpToPage(_selectedItem);
+        });
+      },
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Color(0x44e8e8ff),
+        ),
+        height: 60.0,
+        width: MediaQuery.of(context).size.width / 4,
+        child: Column(
+          children: [
+            const SizedBox(height: 10.0),
+            Container(
+              //color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(7.0),
+                child: SvgPicture.asset(
+                  icon,
+                  height: 20.0,
+                  width: 20.0,
+                  allowDrawingOutsideViewBox: true,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10.0),
           ],
         ),
       ),
     );
   }
 
-  Widget buildBottomNav({
-    required String icon,
-    required int index,
-    VoidCallback? onClicked
-  }) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Color(0x44e8e8ff),
-      ),
-      height: 60.0,
-      width: MediaQuery.of(context).size.width / 4,
-      child: Column(
-        children: [
-          const SizedBox(height: 10.0),
-          Container(
-            //color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(7.0),
-              child: SvgPicture.asset(
-                icon,
-                height: 20.0,
-                width: 20.0,
-                allowDrawingOutsideViewBox: true,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          const SizedBox(height: 10.0),
-        ],
-      ),
-    );
+  void _onPageChange(int index) {
+    setState(() {
+      _selectedItem = index;
+    });
   }
 }
